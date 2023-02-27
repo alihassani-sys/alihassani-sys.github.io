a914f91bf19860dbac83ea4b58e4c423ff4e7a4c
alihassani-sys <119749337+alihassani-sys@users.noreply.github.com>
2023-02-28 02:20:18 +1100

    Portfolio

---
+++ script.js
@@ -0,0 +1,39 @@
+
+var date1 = new Date('2000/01/01 00:00:00');
+var seconds = date1.getTime() ;
+
+let date2 = Date.now() ;
+
+
+
+let diff = date2 - date1;
+
+
+
+
+
+
+
+
+ function hello(){
+    document.getElementById("here").innerHTML =  ~~(diff/1000) + " Seconds!";
+    }
+
+
+
+
+
+const aliHassani = document.getElementById("header1");
+
+
+
+ function helloafter(){
+    document.getElementById("here").innerHTML =  "";
+}    
+
+let myName = document.getElementById("header1");
+
+
+
+ setInterval(helloafter,5000);
+
